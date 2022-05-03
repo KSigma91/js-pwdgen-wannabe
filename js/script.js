2b0c6a5459b8ca903aa2e6ded5f06937b7e90748
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-03 16:37:38 +0200

    setup

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// dichiarazione
+let miaStringa;
+
+// assegnazione
+miaStringa = "Qual è il tuo nome?";
+
+console.log(miaStringa);
+
+
+// dichiarazione
+let miaStringa2;
+
+// assegnazione
+miaStringa2 = "Qual è il tuo cognome?";
+
+console.log(miaStringa2);
+
+
+// dichiarazione
+let miaStringa3;
+
+// assegnazione
+miaStringa3 = "Qual è il tuo colore preferito?";
+
+console.log(miaStringa3);
