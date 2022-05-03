77dccd2aa5753a1e54b2e0217efe2cc3c205fb3a
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-03 17:32:58 +0200

    modifica

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// dichiarazione
+// pongo queste domande all'utente
 const nome = prompt("Qual è il tuo nome?");
 const cognome = prompt("Qual è il tuo cognome?");
 const colore = prompt("Qual è il tuo colore preferito?");
 
-// assegnazione
-document.getElementById("mio_id").innerHTML = `Salve ${nome + cognome + colore}`;
+/* questo genererà la password con i risultati della digitazione
+da parte dell'utente
+*/
+document.getElementById("mio_id").innerHTML = `${nome + cognome + colore}21`;
 
 console.log(
     `
